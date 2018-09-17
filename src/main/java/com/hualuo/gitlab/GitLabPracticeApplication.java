bb8013572a7e69447a73a5a676d6d519aadceb22
linhualuo <linhualuo@gmail.com>
2018-09-18 02:30:32 +0800

    first commit

---
+++ src/main/java/com/hualuo/gitlab/GitLabPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.hualuo.gitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLabPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitLabPracticeApplication.class, args);
+        System.out.println("Hello World");
+    }
+}
