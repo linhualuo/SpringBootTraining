056ca88d01f6adf6ef7f5f03625f97ed741cd6d5
linhualuo <linhualuo@gmail.com>
2018-12-01 17:53:42 +0800

    集成eclipse link jpa

---
+++ src/main/java/com/hualuo/config/JpaConfig.java
@@ -0,0 +1,37 @@
+package com.hualuo.config;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+import org.springframework.transaction.jta.JtaTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class JpaConfig extends JpaBaseConfiguration {
+
+    protected JpaConfig(DataSource dataSource, JpaProperties properties,
+                        ObjectProvider<JtaTransactionManager> jtaTransactionManager,
+                        ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers) {
+        super(dataSource, properties, jtaTransactionManager, transactionManagerCustomizers);
+    }
+
+    @Override
+    protected AbstractJpaVendorAdapter createJpaVendorAdapter() {
+        return new EclipseLinkJpaVendorAdapter();
+    }
+
+    @Override
+    protected Map<String, Object> getVendorProperties() {
+        final Map<String, Object> ret = new HashMap<String, Object>();
+        ret.put("eclipselink.weaving", "false");
+        ret.put("eclipselink.ddl-generation", "create-or-extend-tables");
+        return ret;
+    }
+}
